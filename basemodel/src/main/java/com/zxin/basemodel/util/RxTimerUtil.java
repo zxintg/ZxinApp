5a9930a422088857ab367d8e87d63b124af56d38
Kui Liu 刘奎 <kui.liu.o@nio.com>
2019-05-08 17:44:01 +0800

    添加工具类

---
+++ basemodel/src/main/java/com/zxin/basemodel/util/RxTimerUtil.java
@@ -0,0 +1,104 @@
+package com.zxin.basemodel.util;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.RectF;
+import com.zxin.root.util.logger.LogUtils;
+import java.util.concurrent.TimeUnit;
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+
+/******
+ * RX 工具类
+ */
+public class RxTimerUtil {
+    private final static LogUtils.Tag TAG = new LogUtils.Tag("RxTimerUtil");
+    private static Disposable mPollingDisposable;
+    private static Disposable mDelayDisposable;
+    private static final long TIME_OUT = 7000;
+
+    /*****
+     * 轮询 执行
+     * @param timmer 轮询时间
+     */
+    public static void schedulePollingTimer(int timmer) {
+        Observable.interval(timmer, TimeUnit.SECONDS, Schedulers.io()).
+                subscribeOn(Schedulers.io())
+                .subscribe(new Observer<Long>() {
+            @Override
+            public void onSubscribe(Disposable disposable) {
+                LogUtils.i(TAG, "polling task : schedule");
+                mPollingDisposable = disposable;
+            }
+
+            @Override
+            public void onNext(Long aLong) {
+                LogUtils.i(TAG, "polling task : onNext");
+
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                LogUtils.printStackTrace(TAG, throwable);
+                cancelPollingTimer();
+            }
+
+            @Override
+            public void onComplete() {
+                LogUtils.i(TAG, "polling task: complete");
+                cancelPollingTimer();
+            }
+        });
+    }
+
+    /*****
+     * 单次执行 延迟
+     * @param delay 延迟时间
+     */
+    public static void scheduleDelayTimer(int delay) {
+        Observable.timer(delay, TimeUnit.MILLISECONDS, Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<Long>() {
+                    @Override
+                    public void onSubscribe(Disposable disposable) {
+                        LogUtils.i(TAG, "delay task: schedule");
+                        mDelayDisposable = disposable;
+                    }
+
+                    @Override
+                    public void onNext(Long aLong) {
+                        LogUtils.i(TAG, "delay task: onNext");
+                    }
+
+                    @Override
+                    public void onError(Throwable throwable) {
+                        LogUtils.printStackTrace(TAG, throwable);
+                        cancelDelayTimer();
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        LogUtils.i(TAG, "delay task : complete");
+                        cancelDelayTimer();
+                    }
+                });
+    }
+
+    public static void cancelPollingTimer() {
+        if (mPollingDisposable != null && !mPollingDisposable.isDisposed()) {
+            mPollingDisposable.dispose();
+            LogUtils.i(TAG, "polling : cancelPollingTimer");
+        }
+    }
+
+    public static void cancelDelayTimer() {
+        if (mDelayDisposable != null && !mDelayDisposable.isDisposed()) {
+            mDelayDisposable.dispose();
+            LogUtils.i(TAG, "delay : cancelDelayTimer");
+        }
+    }
+
+}
